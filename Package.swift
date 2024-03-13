5fd73e9fa3123869ec6c1ed880219bca6f7aede4
Alon Shprung <alonshp1@gmail.com>
2024-03-13 11:55:14 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,10 +12,6 @@ let package = Package(
     dependencies: [
         // Here we define our package's external dependencies
         // and from where they can be fetched:
-        .package(
-            url: "https://github.com/ReactiveX/RxSwift.git",
-            .upToNextMinor(from: "6.5.0")
-        )
     ],
     targets: [
         .binaryTarget(
@@ -26,9 +22,6 @@ let package = Package(
             name: "OpenWebSDKWrapperTarget",
             dependencies: [
                 .target(name: "OpenWebSDK", condition: .when(platforms: .some([.iOS]))),
-                .product(name: "RxSwift-Dynamic", package: "RxSwift"),
-                .product(name: "RxCocoa-Dynamic", package: "RxSwift"),
-                .product(name: "RxRelay-Dynamic", package: "RxSwift"),
             ],
             path: "OpenWebSDKWrapperTarget"
         )
